81ab3b6b61df9998dffcb225d4b68dad474fd8d9
Arwalk <SirAgentProvocateurArwalk@gmail.com>
2024-04-20 20:17:57 +0200

    fix directory

--- util/all_archives.py
+++ util/all_archives.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     as_json = json.loads(data)
     urls = [x['u'] for x in as_json]
 
-    logging.basicConfig(filename=f"{args.output}{os.path.sep}/errors.log", level=logging.DEBUG,
+    logging.basicConfig(filename=f"{args.output[0]}{os.path.sep}/errors.log", level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s %(name)s %(message)s')
     logger = logging.getLogger("all_lp_archive_to_epub")
 
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     for url in urls:
         try:
             lparchive2epub(tqdm, "https://lparchive.org/" + url,
-                           f"{args.output}{os.path.sep}{url.replace('/', '')}.epub")
+                           f"{args.output[0]}{os.path.sep}{url.replace('/', '')}.epub")
         except Exception as e:
             pbar.write(f"failed to download {url}")
             logger.error(e)
